6efb9fff56a01cf418a7a56a397de2558222bdbe
kennyaja <kenny776699@gmail.com>
2023-09-15 13:38:14 +0700

    added function to store the click count and some misc things

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const counterElement = document.getElementById("counter");
-let count = 0;
+let count = localStorage.getItem("clickCount") ?? 0;
+if (count != 0) {
+	counterElement.innerText = `you have clicked the koyuki ball ${count} time(s)`;	
+}
 
 function playAudio(audio) {
 	audio.play();
 	count++;
+	localStorage.setItem("clickCount", count)
 	counterElement.innerText = `you have clicked the koyuki ball ${count} time(s)`;	
 }
